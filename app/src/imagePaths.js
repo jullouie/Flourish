57da2d5c580f99286bb7ad6b0266f64cfcd50981
Kellie Au <kjad2022@mymail.pomona.edu>
2024-04-13 22:08:07 -0700

    fixed

--- app/src/imagePaths.js
+++ app/src/imagePaths.js
@@ -1,29 +1,17 @@
-import plant1 from "./pic/plant1.png";
-import plant2 from "./pic/plant2.png";
-import plant3 from "./pic/plant3.png";
-import plant4 from "./pic/plant4.png";
-import plant5 from "./pic/plant5.png";
-import plant6 from "./pic/plant6.png";
-import plant7 from "./pic/plant7.png";
-import plant8 from "./pic/plant8.png";
-import plant9 from "./pic/plant9.png";
-import plant10 from "./pic/plant10.png";
-import plant11 from "./pic/plant11.png";
-import plant12 from "./pic/plant12.png";
 
 const plantImages = [
-    "./pic/plant1.png",
-    "./pic/plant2.png", 
-    "./pic/plant3.png", 
-    "./pic/plant4.png", 
-    "./pic/plant5.png", 
-    "./pic/plant6.png",
-    "./pic/plant7.png", 
-    "./pic/plant8.png",
-    "./pic/plant9.png", 
-    "./pic/plant10.png",
-    "./pic/plant11.png",
-    "./pic/plant12.png"
+    "./pics/plant1.png",
+    "./pics/plant2.png", 
+    "./pics/plant3.png", 
+    "./pics/plant4.png", 
+    "./pics/plant5.png", 
+    "./pics/plant6.png",
+    "./pics/plant7.png", 
+    "./pics/plant8.png",
+    "./pics/plant9.png", 
+    "./pics/plant10.png",
+    "./pics/plant11.png",
+    "./pics/plant12.png"
     // Add more image paths as needed
   ];
 
